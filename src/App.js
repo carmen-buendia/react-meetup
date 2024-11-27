bf007c29eb1e2071c6603b92dd20d04afb6ca4d7
unknown <cbuendia@laudus.cl>
2024-11-27 22:31:26 +0100

    Modify App.file

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetupsPage";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupsPage from "./pages/NewMeetup";
-import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./utils/constants";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import { MeetupsProvider } from "./context/MeetupsContext";
 
 import MainNavigation from "./components/layout/MainNavigation";
 import Layout from "./components/layout/Layout";
 
 function App() {
-  const [page, setPage] = useState(ALL_MEETUP_PAGE);
-
-  function getCurrentPageComponent() {
-    let currentPageComponent = <AllMeetupsPage />;
-    switch (page) {
-      case FAVORITES_PAGE:
-        currentPageComponent = <FavoritesPage />;
-        break;
-      case NEW_MEETUP_PAGE:
-        currentPageComponent = <NewMeetupsPage />;
-        break;
-      default:
-        currentPageComponent = <AllMeetupsPage />;
-    }
-
-    return currentPageComponent;
-  }
-
   return (
     <div data-test="app">
-      <MainNavigation setPage={setPage} />
-      <Layout>{getCurrentPageComponent()}</Layout>
+      <Router>
+        <FavoritesProvider>
+          <MeetupsProvider>
+            <MainNavigation />
+            <Layout>
+              <Switch>
+                <Route path="/" exact component={AllMeetupsPage} />
+                <Route path="/new-meetup" component={NewMeetupsPage} />
+                <Route path="/favorites" component={FavoritesPage} />
+              </Switch>
+            </Layout>
+          </MeetupsProvider>
+        </FavoritesProvider>
+      </Router>
     </div>
   );
 }
