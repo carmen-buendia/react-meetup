15aba3953154423e34bb10717b4006eadc0af273
unknown <cbuendia@laudus.cl>
2024-11-27 22:32:13 +0100

    Create navLinks constant in utils folder

---
+++ src/utils/navLinks.js
@@ -0,0 +1,5 @@
+export const navLinks = [
+  { to: "/", text: "All Meetups", testId: "all-meetups-link" },
+  { to: "/new-meetup", text: "Add New Meetup", testId: "new-meetup-link" },
+  { to: "/favorites", text: "My Favorites", testId: "favorites-link" },
+];
